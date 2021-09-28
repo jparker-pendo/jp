f81e37341b613e71e3865bd098412499830cc619
jparker-pendo <78813271+jparker-pendo@users.noreply.github.com>
2021-09-28 10:34:22 -0400

    Update pendo.js
    updating visitor/account ID

--- pendo.js
+++ pendo.js
@@ -1,55 +1,39 @@
 //Pendo Scripts
 
 (function(apiKey){
-    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=[];
-    v=['initialize','identify','updateOptions','pageLoad','trackEvents'];for(w=0,x=v.length;w<x;++w)(function(m){
+    (function(p,e,n,d,o){var v,w,x,y,z;o=p[d]=p[d]||{};o._q=o._q||[];
+    v=['initialize','identify','updateOptions','pageLoad','track'];for(w=0,x=v.length;w<x;++w)(function(m){
         o[m]=o[m]||function(){o._q[m===v[0]?'unshift':'push']([m].concat([].slice.call(arguments,0)));};})(v[w]);
         y=e.createElement(n);y.async=!0;y.src='https://cdn.pendo.io/agent/static/'+apiKey+'/pendo.js';
         z=e.getElementsByTagName(n)[0];z.parentNode.insertBefore(y,z);})(window,document,'script','pendo');
-        //Call this whenever information about your visitors becomes available
-        //Please use Strings, Numbers, or Bools for value types.
-// pendo.isReady(console.log("is ready"))
+
+        // Call this whenever information about your visitors becomes available
+        // Please use Strings, Numbers, or Bools for value types.
         pendo.initialize({
-//             disablecookies: true,
             visitor: {
-                id:  'drift-visitor',
-                email: 'email@email.com',
-                name: 'my name',
-                emailid: 'visitor-1@pendo.io',   // Required if user is logged in
-                role: 'Admin',
-                return_url: 'https://www.google.com',
-                
-                //drift info
-                
-                userId: 'drift-visitor',
-                
-                
-                //enabledIntegrationVisitors: ['microsoft', 'enforce_modules','blackberry_modules'],
-                //role: ["Guardian"]
-                // email:        // Optional
+                id:    'VISITOR-UNIQUE-ID'
+                email: 'email@email.com'
+                full_name:    'my name'
                 // role:         // Optional
+
                 // You can add any additional visitor level key-values here,
                 // as long as it's not one of the above reserved names.
             },
+
             account: {
-                id: "drift-account"     ,     // Highly recommended
-                accountfield:  '100',
-                boolean_flag: false
+                id:           'ACCOUNT-UNIQUE-ID' // Required if using Pendo Feedback
                 // name:         // Optional
+                // is_paying:    // Recommended if using Pendo Feedback
+                // monthly_value:// Recommended if using Pendo Feedback
                 // planLevel:    // Optional
                 // planPrice:    // Optional
                 // creationDate: // Optional
+
                 // You can add any additional account level key-values here,
                 // as long as it's not one of the above reserved names.
-            },
-      	 //   parentAccount: {
-      	  //    id:  'Parent Account 4',
-      	  //     name:   "enabled integration test"      // Optional
-      	      // You can add any additional master account level
-      	      // key-values here.
-      	    //}
+            }
         });
-      })('fd2507d5-0da6-47c2-5189-f087c06e6922');
+})('b4b565c4-3dc8-4a1a-400d-721f16c3a693');
 
 
 //EU Pendo Install
